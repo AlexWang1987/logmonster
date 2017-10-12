da9e3e75ac1558b560ef23c3f9044a5a94dabf20
AlexWang <1669499355@qq.com>
2017-10-12 10:55:34 +0800

    REAMME

---
+++ README.md
@@ -0,0 +1,43 @@
+# logMonster
+
+Post your app logs(JSON format) to server with a nice limited buffer.
+
+## options (default)
+
+```javascript
+  // server log endpoint
+  endPoint: 'http://localhost:8080/log.html',
+  postMethod: 'POST',
+  postTimeout: 15 * 1000,
+  // buffer limit
+  discardMax: 100 * 1000,
+  // pack max-number of entries at a time
+  entriesMax: 10,
+  concurrentMax: 5,
+  // sleep a little bit
+  concurrentInterval: 3000,
+```
+
+
+## api
+
+- config(options)
+- push(object/string/number)
+
+## demo
+
+```javascript
+import push,{config} from 'logmonster';
+
+// specify your own log endpoint.
+config({
+  endPoint: 'http://192.168.2.10/es/log' // you need to set this most likely.
+})
+
+// and enjoy pushing your JSON log object anywhere.
+push({
+  mod: 'login',
+  contenxt: 'xxxx',
+  url: 'xxxx'
+})
+```
--- src/index.js
+++ src/index.js
@@ -8,17 +8,155 @@
 //  Author: AlexWang
 //  Date: 2017-03-17 11:13:14
 //  QQ Email: 1669499355@qq.com
-//  Last Modified time: 2017-09-20 11:36:39
+//  Last Modified time: 2017-10-12 10:36:13
 //  Description: wbp-init-umd-main
 //
 // //////////////////////////////////////////////////////////////////////////////
 if (WBP_DEV && module.hot) { module.hot.accept(); }
 
-(async function main() {
-  try {
-    console.log('HAPPY CODING YEAH!');
+import localforage from 'localforage';
+import Promise from 'bluebird';
+import cuid from 'cuid';
 
-  } catch (e) {
-    console.error('MAIN ERROR->', e);
+let defaultOptions = {
+  _localForge: localforage.createInstance({
+    name: 'LogMonster',
+    driver: [localforage.INDEXEDDB],
+    storeName: 'dataStore'
+  }),
+  endPoint: 'http://localhost:8080/log.html',
+  postMethod: 'POST',
+  postTimeout: 15 * 1000,
+  discardMax: 100 * 1000,
+  entriesMax: 10,
+  concurrentMax: 5,
+  concurrentInterval: 3000,
+}
+
+function config(options) {
+  defaultOptions = Object.assign(defaultOptions, options);
+}
+
+async function push(item) {
+  const _localForge = defaultOptions._localForge;
+  const discardMax = defaultOptions.discardMax;
+
+  if (_localForge) {
+    const localLength = await _localForge.length();
+    // warning: discard all
+    if (discardMax <= localLength) {
+      await _localForge.clear();
+      // const removeKey = await _localForge.key(0);
+      // if (removeKey) {
+      //   await _localForge.removeItem(removeKey);
+      // }
+    }
+    return await _localForge.setItem(cuid(), item);
   }
-}());
+  console.warn('internal localforage is broken.');
+}
+
+export {
+  push as
+  default,
+  config
+}
+
+async function concurrenInterval() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, defaultOptions.concurrentInterval);
+  })
+};
+
+(async function logMonster() {
+  if (!defaultOptions.endPoint) return console.error('endPoint is missing.');
+  await concurrenInterval();
+  await distributeEntires();
+  await logMonster();
+})();
+
+if (WBP_DEV) {
+  window._push = push;
+  window._config = config;
+  setInterval(() => {
+    _push(Math.random() * 300000000)
+  }, 500)
+}
+
+async function distributeEntires() {
+  const entriesMax = defaultOptions.entriesMax;
+  const concurrentMax = defaultOptions.concurrentMax;
+  const _localForge = defaultOptions._localForge;
+  const batchPackages = [];
+
+  for (let i = 0; i < concurrentMax; i++) {
+    const batchIndex = i * entriesMax;
+    const batchKeyAvailable = await _localForge.key(batchIndex);
+
+    if (!batchKeyAvailable) break;
+
+    const entryPackage = [];
+
+    for (let j = 0; j < entriesMax; j++) {
+      const entryIndex = batchIndex + j;
+      const entryKeyAvailable = await _localForge.key(entryIndex);
+
+      if (!entryKeyAvailable) break;
+
+      const entryKeyValue = await _localForge.getItem(entryKeyAvailable);
+      if (entryKeyValue) {
+        entryPackage.push({
+          key: entryKeyAvailable,
+          value: entryKeyValue
+        })
+      }
+    }
+
+    if (entryPackage.length) {
+      batchPackages.push(entryPackage);
+    }
+  }
+
+  if (batchPackages.length) {
+    const endPoint = defaultOptions.endPoint;
+    const postMethod = defaultOptions.postMethod;
+    const postTimeout = defaultOptions.postTimeout;
+    return Promise.map(batchPackages, (batchPackage) => Promise.try(() => {
+      const batchValues = batchPackage.map((entry) => entry.value);
+      let batchCached = false;
+      return new Promise((rsv) => {
+          try {
+            fetch(endPoint, {
+                method: postMethod,
+                headers: {
+                  'Content-type': 'application/json'
+                },
+                body: JSON.stringify(batchValues)
+              })
+              .then((postRes) => {
+                if (!batchCached) {
+                  if (postRes.ok) {
+                    return rsv(Promise.map(batchPackage, ({ key }) => _localForge.removeItem(key)))
+                  }
+                  console.error('BATCH PACKAGE SERVER ERROR ->', `[${postMethod}] [${postRes.status}] [${postRes.statusText}] ${postRes.url}`);
+                }
+                rsv();
+              })
+              .catch((networkError) => {
+                console.error('BATCH PACKAGE NETWORK ERROR ->', networkError);
+                rsv();
+              })
+          } catch (err) {
+            console.error('FETCH POST INTERVAL ERROR ->', err);
+            rsv();
+          }
+        })
+        .timeout(postTimeout)
+        .catch((err) => {
+          batchCached = true;
+          console.error('BATCH PACKAGE TIMEOUT ->', err);
+        })
+    }))
+  }
+  // console.log('batchPackages is zero.');
+}
